d61891b3d22044e97793c67a67a9ba529abce281
Rajesh Danala <rajesh.danala@gmail.com>
2015-10-12 00:49:53 -0400

    commneted latency simulation

--- client/templates/posts/post_submit.js
+++ client/templates/posts/post_submit.js
@@ -19,8 +19,8 @@ Template.postSubmit.events({
         alert('This Link has already been posted');
       }
       //Router.go('postPage',{_id:result._id});
-
+      Router.go('postsList');
     });
-    Router.go('postsList');
+
   }
 });
