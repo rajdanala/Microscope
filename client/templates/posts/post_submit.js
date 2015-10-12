344713268934b94b6bbf4b50cd4bcb8eea327b1a
Rajesh Danala <rajesh.danala@gmail.com>
2015-10-12 00:23:50 -0400

    * Meteor.methods({}) for server side methods * Meteor.call(name,data,function) for client side * meteor add check - to security check * _.extend() underscore extend another obj props * link check

---
+++ client/templates/posts/post_submit.js
@@ -0,0 +1,23 @@
+Template.postSubmit.events({
+  'submit form': function(e) {
+    e.preventDefault();
+
+    var post = {
+      url: $(e.target).find('[name=url]').val(),
+      title: $(e.target).find('[name=title]').val()
+    };
+
+    //post._id = Posts.insert(post);
+    //Router.go('postPage', post);
+
+    Meteor.call('postInsert', post, function(error, result){
+      if(error){
+        console.log('error', error);
+        alert(error.reason);
+      }
+      if(result){
+        Router.go('postPage',{_id:result._id});
+      }
+    });
+  }
+});
